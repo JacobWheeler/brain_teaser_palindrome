0573815a3d5f20bd2d75b76307d4915f7eba8712
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-09 10:35:12 -0600

    brain teaser starting

---
+++ palindrome.rb
@@ -0,0 +1,34 @@
+def input
+    puts "Please enter your text..."
+    user_input = gets.split("").map { |s| s.to_s }
+    [] <<
+   
+    if
+    [0] == [-1]
+    puts "You have entered a palindrome"
+    else
+    puts "That's a negatory ghost rider."
+    end
+input
+end
+
+
+
+
+input
+
+# User Input
+# reversed_str = ''
+# user_input = "racecar"
+# loop backwards through the user_input string and push each letter onto the resversed string
+# HINT: .downto method
+# once you have user_input and reverse_str check for equality
+
+def input
+    puts "Please enter text..."
+    user_input = gets.strip
+    reversed_str = ''
+    user_input.downto 
+
+end
+
